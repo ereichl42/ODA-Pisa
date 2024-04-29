9de57592af1dd1ba5a87af61fae34b1c54228430
Thomas Hegedues <thohe652@student.liu.se>
2024-04-29 16:58:33 +0200

    Cleaned up the Master branch

---
+++ tests/ReadME.txt
@@ -0,0 +1,3 @@
+This is an unnecessary file, just for learning purposes.
+
+Test files go in this folder.
--- tests/load_finance_data_TH.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-# Data source files are in the data/ directory in the root of the project
-filepath = 'data/estat_educ_uoe_fina01.tsv'
-
-
-# Read TSV file
-data = pd.read_csv(filepath, sep='\t', encoding='utf-8')
