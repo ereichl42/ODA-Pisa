9f0a9a3cb60803e9e458c612b8efcf4eac7068c9
Thomas Hegedues <thohe652@student.liu.se>
2024-06-07 13:45:26 +0200

    Overworked, bugs solved. Tested with the excel, and works now.

--- tests/load_PISA_data_fromExcel.py
+++ tests/load_PISA_data_fromExcel.py
@@ -17,72 +17,9 @@ if not os.path.exists(file_path):
     exit()
 
 # Read the Excel file, figure out how many tabs
+# Tab names need to be stored to access later
 xls = pd.ExcelFile(file_path)
 tabs = xls.sheet_names
-
-# In the first few lines of each tab there is always a description if the data is about mathematics, reading, or science
-# Thus I can search for it and add it as info to the dictionary
-# I will use the first tab as a test tab
-
-keywords = ['mathematics', 'reading', 'science']
-tab_info = {}
-for tab in tabs:
-    df = pd.read_excel(file_path, sheet_name=tab)
-
-    resultType = None
-
-    for keyword in keywords:
-        for _, row in df[:10].iterrows():
-            if row.str.contains(keyword, case=False).any():
-                resultType = keyword
-                break
-        if resultType:
-            break
-
-    tab_info[tab] = resultType
-
-# print(tab_info)
-
-
-# In this part the actual data is read from each tab.
-# Again, it is not clear where the data is, the actual table within the excel's tab.
-# But it is known how the header looks like, so we search for it and then read the data from there.
-
-# The header is: 'Year/Study', 'Jurisdiction', Average', 'Standard Error'
-# Each is in a separate cell.
-
-header = ['Year/Study', 'Jurisdiction', 'Average', 'Standard Error']
-
-# This header can be found in the first 15 rows of the tab
-# The data is below it
-
-data = {}
-
-for tab in tabs:
-    df = pd.read_excel(file_path, sheet_name=tab)
-
-    # Find the header
-    header_row = None
-    for i, row in df[:15].iterrows():
-        if row.str.contains(header[0], case=False).any() and \
-                row.str.contains(header[1], case=False).any() and \
-                row.str.contains(header[2], case=False).any() and \
-                row.str.contains(header[3], case=False).any():
-            header_row = i
-            break
-
-    if header_row is None:
-        print('Header not found in tab:', tab)
-        continue
-
-    # Read the data till a line is empty
-    for i, row in df[header_row + 1:].iterrows():
-        if row.str.contains('nan', case=False).all():
-            data[tab] = df[header_row + 1:i]
-            break
-
-
-# Combine all the above
 
 # The combined excel files with multiple tabs contain the data but with additonal info.
 # The data does not start with first cell in each tab. Thus, searching for certain info and the actual data is needed.
@@ -99,55 +36,61 @@ for tab in tabs:
     # In the first few lines of each tab there is always a description if the data is about mathematics, reading, or science
     # Search for the type of results
     resultType = None
-    info_row = None
-    for keyword in keywords:
-        for i, row in df[:10].iterrows():
-            if row.str.contains(keyword, case=False).any():
-                resultType = keyword
-                break
-        if resultType:
-            info_row = i
-            break
-
-    # Some safety check, just in case the online source of the file changed its format
-    if resultType is None:
-        print('ERROR: No info found in tab! Maybe the downloaded files is faulty!')
-        exit()
-
-    # The resultType is the key in the data dictionary
-    data[resultType] = {}
-
-    # Find the header
-    # It should be a few lines below the info
     header_row = None
-    for i, row in df[info_row:info_row + 15].iterrows():
-        # Assuming that the online source might change the header a bit,
-        # I just check that at least two words from the header are in the row
-        keywordsFound = 0
-        for keyword in header:
-            if row.str.contains(keyword, case=False).any():
-                keywordsFound += 1
-        if keywordsFound >= 2:
-            header_row = info_row + i
+    end_row = None
+
+    # I combine everything in one loop, and just check if resultType was already found
+    for i, row in df[:].iterrows():
+        if resultType == None:
+            for keyword in keywords:
+                if row.str.contains(keyword, case=False).any():
+                    resultType = keyword
+                    break
+
+            # Some safety check, just in case the online source of the file changed its format
+            if i > 10:
+                print('ERROR: No info found in tab:', tab,
+                      'Maybe the downloaded files is faulty!')
+                exit()
+
+        # If the resultType is already found, then search for the header of the data
+        elif header_row == None:
+            # Check if the header of the actual table is in the row
+            # Assuming that the online source might change the header a bit,
+            # I just check that at least two words from the header are in the row
+            keywordsFound = 0
+            for keyword in header:
+                if row.str.contains(keyword, case=False).any():
+                    keywordsFound += 1
+            if keywordsFound >= 2:
+                header_row = i
+
+            # Some safety check, just in case the online source of the file changed its format
+            if i > 15:
+                print('ERROR: Header not found in tab:', tab,
+                      'Maybe the downloaded files is faulty!')
+                exit()
+
+        # If the header is found, then search for the end of the data - an empty row
+        elif row.str.contains('nan', case=False).all():
+            end_row = i
             break
-
     # Some safety check, just in case the online source of the file changed its format
-    if header_row is None:
-        print('ERROR: Header not found in tab! Maybe the downloaded files is faulty!')
-        exit()
+    # It could be that now there is no empty row at the end of the data but instead the data ends at the end of the tab
+    if end_row is None:
+        end_row = i
 
-    # Read the data till a line is empty
-    for i, row in df[header_row:].iterrows():
-        print(row)
-        """ 
-        if row.str.contains('nan', case=False).all():
-            print('Data found in tab:', tab, 'of type:', resultType,
-                  'from row:', header_row + 1, 'to row:', i - 1)
-            data[resultType] = df[header_row + 1:header_row+i]
-            break """
+    # Extract the actual data from header to the end row.
+    # For additonal cleanup I
+    # 1. drop the columns with NaN values (probably empty columns in the beginning and end of the data)
+    # 2. and reset the index
+    df_clean = df[header_row:end_row]
+    df_clean = df_clean.dropna(axis=1, how='all')
+    df_clean = df_clean.reset_index(drop=True)
 
-    # For testing
-    exit()
+    # Store the data in the dictionary with the type of results as the key
+    data[resultType] = df_clean
+
 
 # The data is now in a dictionary with the type of results as the first key.
 # The data itself is still in a pandas DataFrame.
@@ -155,5 +98,7 @@ for tab in tabs:
 # For testing, I print the first few rows of each tab
 for resultType, df in data.items():
     print('Result type:', resultType)
-    print('First few rows of the data:')
+    print('First and last few rows of the data:')
     print(df.head(5))
+    print('...')
+    print(df.tail(5))
