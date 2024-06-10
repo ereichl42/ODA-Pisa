d86e251604eb6c51b3a65b286703b5ef4f8156b4
Thomas Hegedues <thohe652@student.liu.se>
2024-06-10 07:28:17 +0200

    Change structure to web application

--- mainTest.py
+++ mainTest.py
@@ -6,3 +6,8 @@ from utils.load_PISA_data_fromExcel import load_PISA_data_fromExcel
 if __name__ == "__main__":
     test = load_PISA_data_fromExcel(
         "data/pisa_data/original_xls_source_combined_tables/IDEExcelExport-Apr222024-0515PM.xls")
+
+
+# Get project tree
+# python -m pip install tree
+# python -m tree -L 2
